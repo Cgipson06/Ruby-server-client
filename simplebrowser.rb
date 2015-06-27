47c05d100293388434a0c2d7eeac52b0cba19a13
Cody Gipson <CGipson06@yahoo.com>
2015-06-26 22:54:14 -0500

    better output

--- simplebrowser.rb
+++ simplebrowser.rb
@@ -10,45 +10,35 @@ input = gets.chomp.downcase
 puts""
 
 if input == 'get'
-    path = "./index.html"                 # The file we want 
-    request = "GET #{path} HTTP/1.0\r\n\r\n"# This is the HTTP request we send to fetch a file
-    socket = TCPSocket.open(host,port)  # Connect to server
-    socket.print(request)               # Send request
-    response = socket.read              # Read complete response
-    # Split response at first blank line into headers and body
-    headers,body = response.split("\r\n\r\n", 2) 
-    print body 
+  path = "./index.html"                 # The file we want 
+  request = "GET #{path} HTTP/1.0\r\n\r\n"# This is the HTTP request we send to fetch a file
+  socket = TCPSocket.open(host,port)  # Connect to server
+  socket.print(request)               # Send request
+  response = socket.read              # Read complete response
+  # Split response at first blank line into headers and body
+  headers,body = response.split("\r\n\r\n", 2) 
+  print body 
 
-    else
+else
   printf "Please enter the name of the person you would like to sign up:"
   name = gets.chomp
   puts ""
   printf "Please enter #{name}'s email address: "
   email = gets.chomp
   signup = Hash[:viking,Hash[:name, name, :email,email]].to_json
-  puts signup
-  puts JSON.parse(signup)
-
   path = './thanks.html'
-  message = "POST #{path} HTTP/1.0\r\nFrom: Cgipson06@yahoo.com\r\n" +
+  message = "POST #{path} HTTP/1.0\r\nFrom: Cgipson06@aol.hotmail.com\r\n" +
                  "User-Agent: ConsoleBrowser\r\nContent-Type:  application/json\r\n" +
                  "Content-Length: #{signup.size}\r\n\r\n" +
                  "#{signup}\r\n"
-                 
-  
-
   socket = TCPSocket.open(host,port)  # Connect to server
   socket.print(message)  
   response = socket.read
   puts ""
   print "sent POST request:  #{message}"
   puts""
-  
   print response
-
-   
-  
-  end
+end
   
   
 
--- simpleserver.rb
+++ simpleserver.rb
@@ -2,46 +2,46 @@ require 'socket'
 require 'json'
 server = TCPServer.open(80)
 
-
+puts "Simple Ruby Server initialized, "
 loop  {
+    puts "   awaiting connections..."
     client = server.accept
+    puts "Connection established, listening to port 80..."
     action = ""
-    while line = client.gets   # Thanks Tommy Noe for the blog post about this
+    while line = client.gets   # Thanks Tommy Noe for the blog post about this gets loop method
       action += line
-      break if action =~ /\r\n\r\n$/ 
+      break if action =~ /\r\n\r\n$/ #The while will terminate with the end of the get request, but the post needs this to break out the header to get the message size
     end 
-    puts "action: #{action}"
-    body_size = action.split(' ').last.to_i
+    puts "Request recieved:   #{action}"
+    puts ""
+    body_size = action.split(' ').last.to_i  
     body = client.read(body_size)
     packet = action.split(' ')
     if packet[0] == "GET"
-      path = packet[1].to_s   # gotta sanitize or bad things can happen eg '.. .. .. etc/passwords ' 
+      path = packet[1].to_s   # I should really sanitize or bad things can happen eg '.. .. .. etc/passwords ' 
       if File.file?(path)
         f = File.open(path)
         response= f.read
         f.close
+        puts "Sending #{path}"
         client.print "HTTP/1.1 200 OK\r\n" +
-                 "Content-Type: text/plain\r\n" +
-                 "Content-Length: #{response.bytesize}\r\n" +
-                 "Connection: close\r\n"
-        client.print "\r\n"
+                     "Content-Type: text/plain\r\n" +
+                     "Content-Length: #{response.bytesize}\r\n" +
+                     "Connection: close\r\n\r\n"
         client.print response
         client.close
       else #(no file present)
         message = "File not found"
-        # respond with a 404 error code to indicate the file does not exist
         client.print "HTTP/1.1 404 Not Found\r\n" +
-                 "Content-Type: text/plain\r\n" +
-                 "Content-Length: #{message.size}\r\n" +
-                 "Connection: close\r\n"
-
-        client.print "\r\n"
+                     "Content-Type: text/plain\r\n" +
+                     "Content-Length: #{message.size}\r\n" +
+                     "Connection: close\r\n\r\n"  
         client.print message
         client.close
       end
     
     elsif packet[0] == "POST"
-      path = action.split(' ')[1].to_s
+      path = packet[1].to_s
       if File.file?(path)      
          params = JSON.parse(body)
          generated = "<li>Name: #{params['viking']['name']}</li><li>Email: #{params['viking']['email']}</li>"
@@ -50,9 +50,9 @@ loop  {
          f.close
          content_body = file_content.gsub("<%= yield %>", generated)
          client.print "HTTP/1.1 200 OK\r\n" +
-                 "Content-Type: text/plain\r\n" +
-                 "Content-Length: #{content_body.bytesize}\r\n" +
-                 "Connection: close\r\n"
+                      "Content-Type: text/plain\r\n" +
+                      "Content-Length: #{content_body.bytesize}\r\n" +
+                      "Connection: close\r\n"
          client.print(content_body)
          client.close
       end
